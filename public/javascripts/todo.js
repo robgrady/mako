70a3548a1af142d120e05a2a1af5919a74e8af49
Rob Grady <rob@robgrady.com>
2013-06-08 23:57:46 -0500

    Sync Tweaks

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -3,7 +3,6 @@ function TodoCtrl($scope) {
         {text:'learn angular', done:true},
         {text:'build an angular app', done:false}];
 
-
     $scope.addTodo = function() {
         $scope.todos.push({text:$scope.todoText, done:false});
         $scope.todoText = '';
